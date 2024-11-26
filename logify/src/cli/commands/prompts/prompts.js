7f21d8f3232434bf7a6d68ad0ed1d9bf09f78842
Daniel Park <danielpark779@gmail.com>
2024-11-25 20:22:02 -0800

    modify prompts to be more accurate for llm

--- logify/src/cli/commands/prompts/prompts.js
+++ logify/src/cli/commands/prompts/prompts.js
@@ -1,6 +1,6 @@
 function summarize_short(input) {
     return `For the following git log, summarize all changes contained in it that might be important to a user in a one-liner, then summarize all changes in bullet points: ${input}
-    Your output should be in the format:
+    Exclude any changes that are non-functional, such as README, dependencies, formatting, and documentation updates. Your output should be in the format:
     Title
 
     - summarized change 1
